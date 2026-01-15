5cace3ac82537abb7b225762578c31fe632e8fcd
JRC <69986863+JRGCr@users.noreply.github.com>
2026-01-15 16:38:20 -0500

    Add site/config.js

---
+++ site/config.js
@@ -0,0 +1,11 @@
+window.WAITLIST_CONFIG = {
+  "headline": "join!",
+  "description": "deescription",
+  "collect_name": true,
+  "collect_company": false,
+  "primary_color": "#3b82f6",
+  "logo_url": "",
+  "turnstile_enabled": false,
+  "turnstile_site_key": "",
+  "api_url": "WORKER_URL_PLACEHOLDER"
+};
